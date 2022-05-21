549475d1dd97521f247d77ae34f450db96a98a09
Yashas Gowda <yashas348@gmail.com>
2022-05-21 13:42:19 +0530

    update

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const app = express();
 app.get('/', (req, res) => {
    axios
       .get(req.query.url, { responseType: 'arraybuffer' })
-      .then((r) =>
+      .then((r) => {
+         res.header('Access-Control-Allow-Origin', '*');
          res.send({
             success: true,
             url: req.query.url,
             data: `data:image/png;base64,${Buffer.from(r.data, 'binary').toString('base64')}`,
-         })
-      )
+         });
+      })
       .catch(() =>
          res.send({
             success: false,

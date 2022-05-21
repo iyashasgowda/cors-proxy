b28a61a4e7970e90f9344cf492fe2e52d1169056
Yashas Gowda <yashas348@gmail.com>
2022-05-21 12:35:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.get('/', (req, res) => {
+   axios
+      .get(req.query.url, { responseType: 'arraybuffer' })
+      .then((r) =>
+         res.send({
+            success: true,
+            url: req.query.url,
+            data: `data:image/png;base64,${Buffer.from(r.data, 'binary').toString('base64')}`,
+         })
+      )
+      .catch(() =>
+         res.send({
+            success: false,
+            url: req.query.url,
+            data: null,
+         })
+      );
+});
+
+app.listen(3333);

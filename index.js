2e3e66948a1fdd9731f2d5cb05c41b7e86c9348f
Yashas Gowda <yashas348@gmail.com>
2022-07-28 19:59:19 +0530

    #update to index.js
    Signed-off-by: Yashas Gowda <yashas348@gmail.com>

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const axios = require('axios');
 const app = express();
 
 app.get('/', (req, res) => {
-   axios
-      .get(req.query.url, { responseType: 'arraybuffer' })
-      .then((r) => {
-         res.header('Access-Control-Allow-Origin', '*');
-         res.send({
-            success: true,
-            url: req.query.url,
-            data: `data:image/png;base64,${Buffer.from(r.data, 'binary').toString('base64')}`,
-         });
-      })
-      .catch(() =>
-         res.send({
-            success: false,
-            url: req.query.url,
-            data: null,
-         })
-      );
+    axios
+        .get(req.query.url, {responseType: 'arraybuffer'})
+        .then((r) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.send({
+                success: true,
+                url: req.query.url,
+                data: `data:image/png;base64,${Buffer.from(r.data, 'binary').toString('base64')}`,
+            });
+        })
+        .catch(() =>
+            res.send({
+                success: false,
+                url: req.query.url,
+                data: null,
+            })
+        );
 });
 
 app.listen(process.env.PORT || 3333);
